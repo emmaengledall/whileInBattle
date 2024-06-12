9f58df61a2669df246436265a621c4f0f7dc9af8
Emma Engledal <55669@dmjx.dk>
2024-06-12 22:36:34 +0200

    df

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
